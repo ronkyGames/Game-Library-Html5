fda756f31a203d503820eda10f6e27461f7d3e00
Fabio Roncari <fabio.roncari@gmail.com>
2023-04-10 19:03:50 +0000

    Initial commit

---
+++ RonkyGames/Container.js
@@ -0,0 +1,27 @@
+class Container{
+  constructor(){
+    this.pos = {x : 0, y : 0}
+    this.children = []
+  }
+
+  // methods
+  add(child){
+    this.children.push(child)
+    return child
+  }
+
+  remove(child){
+    this.children = this.children.filter(c => c!== child)
+    return child
+  }
+
+  update(dt,t){
+    this.children.forEach(child => {
+      if(child.update){
+        child.update(dt,t)
+      }
+    })
+  }
+}
+
+export default Container
---
+++ RonkyGames/index.js
@@ -0,0 +1,22 @@
+//container
+import Container from "./Container.js"
+//game objects
+import Text from "./Text.js"
+import Texture from "./Texture.js"
+import Sprite from "./Sprite.js"
+//Renderer
+import CanvasRenderer from "./renderer/CanvasRenderer.js"
+//controls
+import KeyControls from "./controls/KeyControls.js"
+import PointerControls from "./controls/PointerControls.js"
+
+export default{
+  CanvasRenderer,
+  Container,
+  Text,
+  Texture,
+  Sprite,
+  //controls
+  KeyControls, 
+  PointerControls
+}
