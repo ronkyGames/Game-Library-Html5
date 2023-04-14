947be0462cac7cbd4ff964e0ba81dac1b4aaa38a
Fabio Roncari <fabio.roncari@gmail.com>
2023-04-14 21:21:29 +0000

    Used new classes and math library to create a scrolling background

--- src/main.js
+++ src/main.js
@@ -28,19 +28,34 @@ ship.update = function(dt,t){
   scale.y = Math.abs(Math.sin(t * 1.33)) + 1
 }
 
+//Add everithing to the scene container
+  scene.add(new Sprite(textures.background))
+
 const buildings = scene.add(new Container())
-const makeRandom = (build, x) => {
+const makeRandom = (b, x) => {
   // Place the building at x position, with random scale
-  
+  b.scale.x = math.randf(1, 3)
+  b.scale.y = math.randf(1, 3)
+  b.pos.x = x
+  b.pos.y = h-b.scale.y * 64
 }
 
+  //Populate buildings and place at a random place from 0 to w
+  for(let x = 0; x < 50; x++){
+    const b = buildings.add(new Sprite(textures.building))
+    makeRandom(b, math.rand(w))
+  }
 
+  scene.add(ship)
 
-
-//Add everithing to the scene container
-scene.add(new Sprite(textures.background))
-
-scene.add(ship)
-game.run()
+// call the loop from the game class
+game.run(dt => {
+  buildings.map(b => {
+    b.pos.x -= 100 * dt
+    if(b.pos.x < -80){
+      makeRandom(b,w)
+    }
+  })
+})
 
 
