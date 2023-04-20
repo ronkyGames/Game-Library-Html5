5f7170bf036237d682459baea81dc971fc64f947
Fabio Roncari <fabio.roncari@gmail.com>
2023-04-20 19:51:34 +0000

    Created class Level to manage Tiles

---
+++ src/Level.js
@@ -0,0 +1,28 @@
+import RonkyGames from "../RonkyGames/index.js"
+const {Texture, TileMap, math} = RonkyGames
+
+const texture = new Texture("res/images/tiles.png")
+
+class Level extends TileMap{
+  constructor(w,h){
+    // tile map setup
+    const tileSize = 32
+    const mapW = Math.floor(w / tileSize)
+    const mapH = Math.floor(h / tileSize)
+
+    //make a random level of tile indexes
+    const level = []
+    for(let y = 0; y < mapH; y++){
+      for(let x = 0; x < mapW; x++){
+        level.push({
+          x:math.rand(5),
+          y:math.rand(2)
+        })
+      }
+    }
+
+    super(level, mapW, mapH, tileSize, tileSize, texture)
+  }
+}
+
+export default Level
--- src/main.js
+++ src/main.js
@@ -1,34 +1,20 @@
 // import libraries
 import RonkyGames from "../RonkyGames/index.js"
 import Squizz from "./entities/Squizz.js"
+import Level from "./Level.js"
 
-const {Game, PointerControls, Container, Texture, TileMap, math} = RonkyGames
+
+const {Game, PointerControls, KeyControls, Container, Texture, TileMap, math} = RonkyGames
 
 // Game setup. code
 const game = new Game(640,320)
 const { scene, w, h } = game
 // controls
 const pointer = new PointerControls(game.renderer.view)
+const controls = new KeyControls()
 
-//Test TileMap
-const texture = new Texture("res/images/tiles.png")
-const tileSize = 32
-const mapW = Math.floor(w / tileSize)
-const mapH = Math.floor(h / tileSize)
-
-//Make a random level of tile indexes
-const level = []
-for(let y = 0; y < mapH; y++){
-  for(let x = 0; x < mapW; x++){
-    level.push({
-      x: math.rand(5),
-      y: math.rand(2)
-    })
-  }
-}
-
-const map = new TileMap(level, mapW, mapH, tileSize, tileSize, texture)
-scene.add(map)
+const level = new Level(w,h)
+scene.add(level)
 
 game.run(()=>{
 })
