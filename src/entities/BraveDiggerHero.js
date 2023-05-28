9ec58e828de48437392ff7b84016a82ef66b04ff
ronkyGames <fabio.roncari@gmail.com>
2023-05-28 21:32:45 +0000

    Player in scene walk and Idle animation, and set orientation

--- src/entities/BraveDiggerHero.js
+++ src/entities/BraveDiggerHero.js
@@ -7,7 +7,7 @@ class BraveDiggerHero extends TileSprite{
   constructor(controls){
     const tileSize = 48
     super(texture,tileSize,tileSize)
-    this.anchor = {x:-tileSize/2, y:-tileSize/2}
+    this.anchor = {x:0, y:0}
     this.pivot = {x: tileSize/2, y: tileSize/2}
     this.speed = 0.15
     // direction of travel
@@ -24,12 +24,17 @@ class BraveDiggerHero extends TileSprite{
   setAnims(){
     const {anims} = this
     anims.add("walk", [0,1,2,3,4].map(x => ({x,y:0})),0.07)
-    anims.add("idle", [{x:0,y:4}, {x:0,y:5}],1)
+    anims.add("idle", [{x:4,y:0}, {x:5,y:0}],1)
   }
 
   setWalk(){
     const {anims} = this
-    anims.play("walk")
+    if(this.direction.x != 0 || this.direction.y != 0){ 
+      anims.play("walk")
+      return true
+    }else{
+      return false
+    }
   }
 
   setIdle(){
@@ -60,35 +65,44 @@ class BraveDiggerHero extends TileSprite{
     
   }
 
-  setOrientation(){
+  setOrientation(w){
     // right
     if(this.direction.x == 1){
+      this.anchor ={x:0,y:0}
       this.rotation = 0
       this.scale.x = 1
       this.scale.y = 1
     }else if(this.direction.x == -1){
+      this.anchor ={x:w/2,y:0}
       this.rotation = 0
       this.scale.x = -1
       this.scale.y = 1
     }else if(this.direction.y == 1){
+      this.anchor = {x:0,y:0}
       this.rotation = 90
       this.scale.x = 1
       this.scale.y = 1
     }else if(this.direction.y == -1){
-      this.rotation = 180
-      this.scale.x = -1
+      this.anchor = {x:0,y:0}
+      this.rotation = -90
+      this.scale.x = 1
       this.scale.y = 1
     }
   }
 
   update(dt,t){
     super.update(dt)
-    const {pos, speed,tileSize, rate, frames} = this
+    const {pos, speed, tileW, tileH} = this
     if(this.controls) this.moveControls()
-    this.setOrientation()
+    // set animation 
+    const walk = this.setWalk()
+    if(!walk) this.setIdle()
+    
+    this.setOrientation(tileW)
     //movement handling
-    pos.x += this.direction.x*dt*(tileSize/speed)
-    pos.y += this.direction.y*dt*(tileSize/speed)
+    pos.x += this.direction.x*dt*(tileW/speed)
+    pos.y += this.direction.y*dt*(tileH/speed)
+    
   }
 }
 
